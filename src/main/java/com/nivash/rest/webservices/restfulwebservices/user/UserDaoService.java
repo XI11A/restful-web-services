bdb3ea470cf8a5aefc3af1ab3bfb4811b8da6aff
Nivash <nivash77.n7@gmail.com>
2022-07-22 17:09:34 +0530

    Learned About how to use Delete Mapping
    Learned About how to use Delete Mapping

--- src/main/java/com/nivash/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/nivash/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Component
@@ -38,4 +39,19 @@ public class UserDaoService {
         }
         return null;
     }
+
+    public User deleteByID(int id){
+        Iterator<User> iterator = users.iterator();
+        while (iterator.hasNext()){
+            User user = iterator.next();
+        }
+        for(User user:users){
+            if(user.getId()==id){
+                iterator.remove();
+                return user;
+            }
+        }
+        return null;
+    }
+
 }
